877ec5f2babb4d380b07b4f8763ede64c2475e95
DeminaDaria <61200143+DeminaDaria@users.noreply.github.com>
2021-02-22 21:25:03 +0300

    Added one more tests AmountEqualsBoundary

--- src/test/java/ru/netology/unitCashback/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unitCashback/CashbackHackServiceTest.java
@@ -25,4 +25,14 @@ public class CashbackHackServiceTest {
 
         assertEquals(actual, expected);
     }
+
+    @Test
+    public void shouldAmountEqualsBoundary() {
+        CashbackHackService hackService = new CashbackHackService();
+
+        int actual = hackService.remain(1000);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
 }
